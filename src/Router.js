5d073299e96960869f536e78d0c4c39b98fcd26a
Berke <forcoding2022v2@gmail.com>
2024-03-12 12:52:26 +0300

    up

--- src/Router.js
+++ src/Router.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { StyleSheet } from "react-native";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createMaterialBottomTabNavigator } from 'react-native-paper/react-navigation';
 
 import Icon from "react-native-vector-icons/Entypo";
 
@@ -15,53 +16,62 @@ import Profile from "./pages/Profile/Profile";
 const Stack = createNativeStackNavigator();
 const Tab = createMaterialBottomTabNavigator();
 
+const DefaultTheme = {
+    "colors": {
+        "secondaryContainer": "white",
+    }
+}
+
 const PlantStack = () => {
     return (
         <Stack.Navigator screenOptions={{
-            headerStyle: {backgroundColor: "#376a4f"},
+            headerStyle: { backgroundColor: "#376a4f" },
             headerTitleAlign: "center",
             headerTintColor: "white",
-            }} >
-            <Stack.Screen name="Main" component={Main} initialParams={Main} options={{title:"Bitki Atlası"}} />
-            <Stack.Screen name="Details" component={Details} options={{title: ""}} />
+        }} >
+            <Stack.Screen name="Main" component={Main} initialParams={Main} options={{ title: "Bitki Atlası" }} />
+            <Stack.Screen name="Details" component={Details} options={{ title: "" }} />
         </Stack.Navigator>
     )
 }
 
 function Router() {
     return (
-        <NavigationContainer>
-            <Tab.Navigator
-                screenOptions={{
-                    headerStyle: {
-                        backgroundColor: 'green',
-                    },
-                }}
-                shifting={true}
-                activeColor="white"
-                inactiveColor="black"
-                barStyle={styles.tabBar}>
-                <Tab.Screen name="PlantStack" component={PlantStack} options={{
-                    title: "Bitkiler",
-                    tabBarIcon: () => (
-                        <Icon name="flower" color={"black"} size={24} />
-                    ),
+        <SafeAreaProvider>
+            <NavigationContainer>
+                <Tab.Navigator
+                    theme={DefaultTheme}
+                    screenOptions={{
+                        headerStyle: {
+                            backgroundColor: 'green',
+                        },
+                    }}
+                    shifting={true}
+                    activeColor="white"
+                    inactiveColor="black"
+                    barStyle={styles.tabBar}>
+                    <Tab.Screen name="PlantStack" component={PlantStack} options={{
+                        title: "Bitkiler",
+                        tabBarIcon: () => (
+                            <Icon name="flower" color={"black"} size={24} />
+                        ),
 
-                }} />
-                <Tab.Screen name="Cart" component={Cart} options={{
-                    title: "Sepet",
-                    tabBarIcon: () => (
-                        <Icon name="shopping-cart" color={"black"} size={24} />
-                    ),
-                }} />
-                <Tab.Screen name="Profile" component={Profile} options={{
-                    title: "Profil",
-                    tabBarIcon: () => (
-                        <Icon name="user" color={"black"} size={24} />
-                    ),
-                }} />
-            </Tab.Navigator>
-        </NavigationContainer >
+                    }} />
+                    <Tab.Screen name="Cart" component={Cart} options={{
+                        title: "Sepet",
+                        tabBarIcon: () => (
+                            <Icon name="shopping-cart" color={"black"} size={24} />
+                        ),
+                    }} />
+                    <Tab.Screen name="Profile" component={Profile} options={{
+                        title: "Profil",
+                        tabBarIcon: () => (
+                            <Icon name="user" color={"black"} size={24} />
+                        ),
+                    }} />
+                </Tab.Navigator>
+            </NavigationContainer >
+        </SafeAreaProvider>
     )
 };
 
